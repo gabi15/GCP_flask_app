2c32c27f4613a42f5836262f81213b38cf176a67
gabi15 <gabcia100@gmail.com>
2022-12-21 21:34:41 +0100

    add db connection

--- app.py
+++ app.py
@@ -1,5 +1,18 @@
-from flask import Flask,Response,jsonify, render_template ,logging,request
+from flask import Flask, jsonify, request, render_template
+from db import get_songs, add_songs
+
 app = Flask(__name__)
+
+@app.route('/songs', methods=['POST', 'GET'])
+def songs():
+    if request.method == 'POST':
+        if not request.is_json:
+            return jsonify({"msg": "Missing JSON in request"}), 400  
+
+        add_songs(request.get_json())
+        return 'Song Added'
+
+    return get_songs()    
 
 @app.route('/')
 def home():
